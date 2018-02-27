c939a4515bb93eb11bdde7fe07df761c5ca4877f
vincenthouyi <vincenthouyi@gmail.com>
2018-02-27 21:55:15 +0800

    mbr implemented

--- fat32/src/mbr.rs
+++ fat32/src/mbr.rs
@@ -1,23 +1,56 @@
-use std::{fmt, io};
+use std::{fmt, io, mem, slice};
 
 use traits::BlockDevice;
 
 #[repr(C, packed)]
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 pub struct CHS {
-    // FIXME: Fill me in.
+    starting_head: u8,
+    starting_sector: u8,
+    starting_cylinder: u8,
+}
+
+impl CHS {
+    pub fn default() -> CHS {
+        CHS {
+            starting_head : 0,
+            starting_sector : 0,
+            starting_cylinder : 0,
+        }
+    }
 }
 
 #[repr(C, packed)]
-#[derive(Debug, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct PartitionEntry {
-    // FIXME: Fill me in.
+    boot_indicator: u8,
+    start_chs: CHS,
+    partition_type: u8,
+    end_chs: CHS,
+    relative_sector: u32,
+    total_sectors: u32,
+}
+
+impl PartitionEntry {
+    pub fn default() -> PartitionEntry {
+        PartitionEntry {
+            boot_indicator : 0,
+            start_chs: CHS::default(),
+            partition_type : 0,
+            end_chs: CHS::default(),
+            relative_sector : 0,
+            total_sectors : 0,
+        }
+    }
 }
 
 /// The master boot record (MBR).
 #[repr(C, packed)]
 pub struct MasterBootRecord {
-    // FIXME: Fill me in.
+    bootstrap: [u8; 436],
+    disk_id: [u8; 10],
+    partition_table: [PartitionEntry; 4],
+    signature: [u8; 2],
 }
 
 #[derive(Debug)]
@@ -31,6 +64,15 @@ pub enum Error {
 }
 
 impl MasterBootRecord {
+    pub fn default() -> MasterBootRecord {
+        MasterBootRecord{
+            bootstrap: [0; 436],
+            disk_id: [0; 10],
+            partition_table: [PartitionEntry::default(); 4],
+            signature: [0; 2],
+        }
+    }
+
     /// Reads and returns the master boot record (MBR) from `device`.
     ///
     /// # Errors
@@ -40,12 +82,37 @@ impl MasterBootRecord {
     /// boot indicator. Returns `Io(err)` if the I/O error `err` occured while
     /// reading the MBR.
     pub fn from<T: BlockDevice>(mut device: T) -> Result<MasterBootRecord, Error> {
-        unimplemented!("MasterBootRecord::from()")
+        let mut mbr = MasterBootRecord::default();
+        let mut mbr_buf = unsafe {
+            slice::from_raw_parts_mut(&mut mbr as *mut MasterBootRecord as *mut u8,
+                                      mem::size_of::<MasterBootRecord>())
+        };
+        if let Err(e) = device.read_sector(0, &mut mbr_buf) {
+            return Err(Error::Io(e));
+        }
+
+        if mbr.signature != [0x55, 0xAA] {
+            return Err(Error::BadSignature);
+        }
+
+        for i in 0..mbr.partition_table.len() {
+            let part_tab = mbr.partition_table[i];
+            if part_tab.boot_indicator != 0 && part_tab.boot_indicator !=  0x80 {
+                return Err(Error::UnknownBootIndicator(i as u8));
+            }
+        }
+
+        Ok(mbr)
     }
 }
 
 impl fmt::Debug for MasterBootRecord {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        unimplemented!("MasterBootRecord::fmt()")
+        f.debug_struct("MasterBootRecord")
+            .field("bootstrap", &"<bootstrap binary>")
+            .field("disk_id", &self.disk_id)
+            .field("partition_table", &self.partition_table)
+            .field("signature", &self.signature)
+            .finish()
     }
 }
